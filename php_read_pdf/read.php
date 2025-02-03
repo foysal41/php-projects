03c8545b6d1167a3af30b67293d06314b75d0416
foysal41 <mdfoysal.mf33@gmail.com>
2025-02-04 02:29:30 +0600

    Up-V2 of php_read_pdf: Check file existence

--- php_read_pdf/read.php
+++ php_read_pdf/read.php
@@ -1,10 +1,17 @@
-<?php 
+<?php
 
 $file = "CV_Foysal_PHP.pdf";
-header('Content-type: application/pdf');
-header('Content-Disposition: inline; filename=" '.$file.' " ');
-header('Content-Transfer-Encoding: binary');
-header('Accept-Ranges: bytes');
-@readfile($file);
-
-?>
+if (file_exists($file)) {
+    header('Content-type: application/pdf');
+    header('Content-Disposition: inline; filename=" ' . $file . ' " ');
+    header('Content-Transfer-Encoding: binary');
+    header('Accept-Ranges: bytes');
+    @readfile($file);
+} else {
+    echo '    <div class="wrapper">
+            <form action="" method="post">
+                <button style="position:absolute; top: 50%;left: 50%; transform: translate(-50%, -50%); height: 50px; width: 220px; background-color: crimson; border: 1px solid black; outline: none;
+                cursor: pointer; font-size: 1.4rem;" >File Not Found</button>
+            </form>
+        </div>';
+}
