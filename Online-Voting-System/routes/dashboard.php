a17ee3affeb1363ce1d69b60cd4cc5f161d3afdc
foysal41 <mdfoysal.mf33@gmail.com>
2025-02-07 23:43:40 +0600

    make register.html page alsoo little working with dashboard.php

---
+++ Online-Voting-System/routes/dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Page</title>
+</head>
+<body>
+    
+    <button>Back</button> 
+    <button>Logout</button>
+<h1>Online Voting System</h1>
+<hr>
+
+
+
+
+</body>
+</html>
